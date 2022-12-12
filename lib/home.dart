34e1cb0c23c41a10d6e7c05067f17a1a836757f0
Sidhant Jadhav <jadhavsidhant90@gmail.com>
2022-12-12 11:10:15 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.black54,
+      alignment: Alignment.center,
+      child: const Text('HomePage',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: Colors.white
+      ),),
+    );
+  }
+}
